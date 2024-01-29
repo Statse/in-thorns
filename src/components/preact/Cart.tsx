89142e186f1eac968201a224dcbb4908d23f38cd
Aleksi Statsevich <aleksi.statsevich@gmail.com>
2024-01-29 13:01:56 +0200

    Update cart styles and export cart type.

--- src/components/preact/Cart.tsx
+++ src/components/preact/Cart.tsx
@@ -5,7 +5,7 @@ import { useStore } from '@nanostores/preact'
 import { useEffect, useState } from 'preact/hooks'
 import { medusa } from '@/scripts/medusa'
 
-type CartType = {
+export type CartType = {
   id: string
   items: {
     id: string
@@ -24,8 +24,6 @@ type CartType = {
 
 export const Cart = () => {
   const [cart, setCart] = useState<CartType | null>(null)
-  // const cart = useStore(persistentCart)
-  // const cartId = useStore(persistentCartId)
   const cartId = useStore(persistentCartId)
 
   useEffect(() => {
@@ -78,7 +76,7 @@ export const Cart = () => {
           <th class='text-start font-thin font-khmer'>Quantity</th>
           <th class='text-start font-thin font-khmer'>Unit Price</th>
           <th class='text-start font-thin font-khmer'>Total</th>
-          <th class='text-start font-thin font-khmer'>Actions</th>
+          <th class='text-start font-thin font-khmer'>Delete</th>
         </tr>
         {cart.items.map((item) => {
           const unitPrice = item.unit_price / 100
@@ -124,10 +122,10 @@ export const Cart = () => {
           )
         })}
         <tr class='h-24 border-t-2 border-white'>
-          <td colspan={4} class='text-end'>
-            <span class='font-khmer text-xl pr-8'>Subtotal:</span>
+          <td colspan={5} class='text-end'>
+            <span class='font-khmer text-3xl pr-8'>Subtotal:</span>
           </td>
-          <td>
+          <td class='text-3xl'>
             <span>{cart.subtotal ? cart.subtotal / 100 : cart.subtotal}€</span>
           </td>
         </tr>
@@ -138,7 +136,7 @@ export const Cart = () => {
         </button>
         <a
           href={`/shop/checkout/${cartId}`}
-          class='px-4 py-2 font-khmer border-2 border-white text-2xl'
+          class='px-4 py-2 font-khmer border-2 border-white text-2xl ml-auto'
         >
           Proceed to checkout
         </a>
