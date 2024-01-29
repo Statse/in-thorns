774da59227334245cad587d033e86514d9499678
Aleksi Statsevich <aleksi.statsevich@gmail.com>
2024-01-29 14:47:55 +0200

    Clean up radio button. Convert css to tailwind.

--- src/components/preact/RadioButton/RadioButton.tsx
+++ src/components/preact/RadioButton/RadioButton.tsx
@@ -7,9 +7,17 @@ type Props = {
 
 function RadioButton({ name, value, title, ...remainingProps }: Props) {
   return (
-    <label class='radiobutton_container font-khmer text-xl'>
-      <input type='radio' name={name} value={value} {...remainingProps} />
-      <span class='checkmark px-4 w-full'> {title}</span>
+    <label class='radiobutton_container relative cursor-pointer font-khmer text-xl'>
+      <input
+        class='absolute opacity-0 cursor-pointer'
+        type='radio'
+        name={name}
+        value={value}
+        {...remainingProps}
+      />
+      <span class='checkmark px-4 w-full bg-black text-white border-2 border-white hover:bg-black hover:text-white hover:border-2 hover:border-white hover:opacity-80'>
+        {title}
+      </span>
     </label>
   )
 }
